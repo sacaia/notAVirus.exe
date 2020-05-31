4dabdd709ab827c03ed46844788fa31a911070b0
Lucas Romani <30930768+sacaia@users.noreply.github.com>
2020-05-31 16:25:04 -0300

    First resource :confetti_ball:

--- js/WHO.populationOver60.js
+++ js/WHO.populationOver60.js
@@ -4,7 +4,7 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 (function (apiUrl, header = "origin: notavirus.biz") {
     function fetchWHS9_92() {
-      return fetch(apiUrl + "?name=Life+expectancy+at+age+60+%28years%29&filter_data=Dim1&value=BTSX")
+      return fetch(apiUrl + "/test")
         .then(function (response) {
           if (response.status === 200)
             console.log(response)
--- pycharm/app.py
+++ pycharm/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, make_response, render_template
+import requests
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
@@ -10,8 +10,12 @@ def hello_world():
 @app.route('/test')
 def test():
     response = make_response(render_template('template.json'))
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
     response.headers['Access-Control-Allow-Origin'] = '*'
-    print(response)
+
+    r = requests.get('https://ghoapi.azureedge.net/api/WHS9_92')
+    response.data = r.content
+    
     return response
 
 
